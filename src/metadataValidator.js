1593a353787fec45a472d0b65dcad9cc59a90ccf
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2026-06-11 16:50:11 -0500

    Add files for metadata validation

--- src/metadataValidator.js
+++ src/metadataValidator.js
@@ -5,7 +5,7 @@ import Ajv from "ajv";
 import addFormats from "ajv-formats";
 import schema from "../schema/hip412-rumi-schema.json" with { type: "json" };
 
-const ajv = new Ajv({ allErrors: true });
+const ajv = new Ajv({ allErrors: true, allowUnionTypes: true });
 addFormats(ajv);
 const validate = ajv.compile(schema);
 
